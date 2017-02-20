668dfa3fa2c9f4a922b72e17569955a3f05a6bc5
jamesjcai <jamescai@genomezoo.net>
2017-02-20 16:09:51 -0600

    .

--- Mahalanobis_Distance/Robust_MD/mahal_robust.m
+++ Mahalanobis_Distance/Robust_MD/mahal_robust.m
@@ -1,21 +1,23 @@
-function [rd]=mahal_robust(X,Y,methodid,alpha)
+function [rd]=mahal_robust(Y,X,Alpha,Methodid)
 % X - Control data matrix (row: samples, column: variables)
 % Y - Case data matrix (row: samples, column: variables)
-if nargin<3, methodid=1; end
-if nargin<4, alpha=0.75; end
+if nargin<4, Methodid=1; end
+if nargin<3, Alpha=0.75; end
+
 n=size(Y,1);
-rd=nan(n,1);    
-switch methodid
+rd=nan(n,1);
+switch Methodid
     case 1
         % new function since v2016a. 2x faster than mcdcov
 	% requirs: size(X,2)<2*size(X,1)
-        [sig,mu] = robustcov(X,'OutlierFraction',1-alpha);
+        [sig,mu] = robustcov(X,'OutlierFraction',1-Alpha);
     case 2
 	% requirs: size(X,2)<=50
-	rew=mcdcov(X,'plots',0,'alpha',alpha);
+	rew=mcdcov(X,'plots',0,'alpha',Alpha);
         mu=rew.center;
         sig=rew.cov;
 end
 for i=1:n
-    rd(i)=sqrt((Y(i,:)-mu)*inv(sig)*(Y(i,:)-mu)');
+     % rd(i)=(Y(i,:)-mu)*inv(sig)*(Y(i,:)-mu)';
+     rd(i)=(Y(i,:)-mu)*(sig\(Y(i,:)-mu)');
 end
