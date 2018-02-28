febd74416e0c8e7d09f8e4b0aca55566b0a0f220
jamesjcai <jamescai@genomezoo.net>
2018-02-28 15:28:16 -0600

    .

--- Linear_mixed_models/s_fitlme_residual_vs_fitted.m
+++ Linear_mixed_models/s_fitlme_residual_vs_fitted.m
@@ -22,5 +22,32 @@
       ds = table(MPG,Weight,Model_Year);
       lme = fitlme(ds,'MPG ~ Weight + (1|Model_Year)')
       
-      
+      lme.randomEffects
 
+      
+%%
+
+%   Example: Fit a model. Simulate new random values for the first
+%            observation.
+     load carsmall
+     ds = dataset(MPG,Weight,Model_Year);
+     lme = fitlme(ds,'MPG ~ Weight + (1|Model_Year)');
+     newds = ds(ones(10000,1),:);
+     r = random(lme,newds);
+     hist(r)
+%
+%      % These random values share a common new random effect due to
+%      % Model_Year, but their variance is comparable to the model value.
+     v1 = var(r)
+     v2 = lme.MSE
+
+%%
+%   Example: Model gas mileage as a function of car weight, with a random
+%            effect due to model year. Compare a model having random
+%            intercepts with one also having random slopes.
+     load carsmall
+     ds = dataset(MPG,Weight,Model_Year);
+     lme = fitlme(ds,'MPG ~ Weight + (1|Model_Year)')
+     lme2 = fitlme(ds,'MPG ~ Weight + (1 | Model_Year) + (-1 + Weight|Model_Year)')
+     compare(lme,lme2)
+      
