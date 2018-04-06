0f17541d713767c5b0d54441497d2fe5ce24193a
James Cai <jamescai@genomezoo.net>
2018-04-06 11:32:22 -0500

    .

--- similarity_factor/pca_sim_factor.m
+++ similarity_factor/pca_sim_factor.m
@@ -1,9 +1,13 @@
 function [s]=pca_sim_factor(Xs,Xh)
 
-L = pca(Xs);
-M = pca(Xh);
+%[coeff1,score1,latent1,tsquared1,explained1] = pca(X1);
+[L,~,~,~,explainedL] = pca(Xs);
+[M,~,~,~,explainedM] = pca(Xh);
 
-k=3;
+k1=find(cumsum(explainedL)>95,1);
+k2=find(cumsum(explainedM)>95,1);
+
+k=max([k1 k2]);
 L=L(:,1:k);
 M=M(:,1:k);
 
