062d0ab140c01026e7066f7f7260fc1328e62d32
James Cai <jamescai@genomezoo.net>
2018-04-12 00:08:47 -0500

    .

--- ellipse/i_95pct_error_ellipse.m
+++ ellipse/i_95pct_error_ellipse.m
@@ -4,11 +4,11 @@ if nargin<3, conf=0.95; end
 
 % Calculate the eigenvectors and eigenvalues
 covariance = cov(datax);
-[~, eigenval] = eig(covariance);
+[eigenvec, eigenval ] = eig(covariance);
 
 % Get the index of the largest eigenvector
-[largest_eigenvec_ind_c] = find(eigenval == max(max(eigenval)));
-%largest_eigenvec = eigenvec(:, largest_eigenvec_ind_c);
+[largest_eigenvec_ind_c,r] = find(eigenval == max(max(eigenval)));
+largest_eigenvec = eigenvec(:, largest_eigenvec_ind_c);
 
 % Get the largest eigenvalue
 largest_eigenval = max(max(eigenval));
@@ -21,6 +21,10 @@ else
     smallest_eigenval = max(eigenval(:,1));
     %smallest_eigenvec = eigenvec(1,:);
 end
+
+
+% Get the coordinates of the data mean
+avg = mean(datax);
 % Get the 95% confidence interval error ellipse
 % chisquare_val = 2.4477;
 chisquare_val = sqrt(chi2inv(conf,2));
@@ -28,5 +32,28 @@ chisquare_val = sqrt(chi2inv(conf,2));
 a=chisquare_val*sqrt(largest_eigenval);
 b=chisquare_val*sqrt(smallest_eigenval);
 % yes=((xy(1)/a)^2 + (xy(2)/b)^2) <= 5.991;
+% yes=((xy(1)/a)^2 + (xy(2)/b)^2) <= chisquare_val^2;
+%yes=(((xy(1)-mm(1))/a)^2 + ((xy(2)-mm(2))/b)^2) <= 1;
 
-yes=((xy(1)/a)^2 + (xy(2)/b)^2) <= chisquare_val^2;
+% Calculate the angle between the x-axis and the largest eigenvector
+angle = atan2(largest_eigenvec(2), largest_eigenvec(1));
+
+% This angle is between -pi and pi.
+% Let's shift it such that the angle is between 0 and 2pi
+if(angle < 0)
+    angle = angle + 2*pi;
+end
+
+% Get the 95% confidence interval error ellipse
+phi = angle;
+X0=avg(1);
+Y0=avg(2);
+a=chisquare_val*sqrt(largest_eigenval);
+b=chisquare_val*sqrt(smallest_eigenval);
+R = [ cos(phi) sin(phi); -sin(phi) cos(phi) ];
+avg1=avg*R;
+X01=avg1(1);
+Y01=avg1(2);
+xynew=xy*R;
+yes=(((xynew(1)-X01)/a)^2 + ((xynew(2)-Y01)/b)^2) <= 1;
+
--- ellipse/test_code.m
+++ ellipse/test_code.m
@@ -1,14 +1,15 @@
 load testdata datax xy
 
-figure;
+fig=figure;
 plot(datax(:,1),datax(:,2),'o')
 hold on
 error_ellipse(cov(datax),mean(datax),'conf',0.95);
 plot(xy(1),xy(2),'*r');
 
-
-
 plot(0.8182,0.066,'*');
+
+dcm_obj = datacursormode(fig);
+set(dcm_obj,'UpdateFcn',{@myupdatefcn,datax})
 
 %%
 
@@ -21,3 +22,18 @@ i_95pct_error_ellipse(datax,mean(datax),0.95)
 % this should return TRUE
 i_95pct_error_ellipse(datax,[0.8182 0.066],0.95)
 
+
+function txt = myupdatefcn(~,event_obj,datax)
+% Customizes text of data tips
+pos = get(event_obj,'Position');
+%I = get(event_obj, 'DataIndex');
+yes=i_95pct_error_ellipse(datax,pos,0.95);
+if yes
+    t='inside';
+else
+    t='outside';
+end
+txt = {['X: ',num2str(pos(1))],...
+       ['Y: ',num2str(pos(2))],...
+       t};
+end
