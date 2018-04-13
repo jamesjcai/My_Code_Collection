ca7da6bbcb44534185e9d9f793546128c98ff485
James Cai <jamescai@genomezoo.net>
2018-04-13 01:24:19 -0500

    .

--- gcta_1.91/s3_mcode_HE_regress/s_extract_triu.m
+++ gcta_1.91/s3_mcode_HE_regress/s_extract_triu.m
@@ -1,6 +1,22 @@
-function [x]=s_extract_triu(X)
-n=size(X,1);
-x=[];
-for k=1:n-1
-    x=[x;diag(X,k)];
+function [x]=s_extract_triu(X,methodid)
+
+if nargin<2, methodid=3; end
+
+switch methodid
+    case 1
+        error('Do not us: output vector element order differs from other methods.')
+        n=size(X,1);
+        x=[];
+        for k=1:n-1
+            x=[x;diag(X,k)];
+        end
+    case 2
+        mask = bsxfun(@lt,[1:size(X,1)]',1:size(X,2));
+        x=X(mask);
+    case 3
+        mask=triu(true(size(X)),1);
+        x=X(mask);
 end
+
+% ref: https://stackoverflow.com/questions/29928468/transfer-the-lower-triangular-part-of-a-matrix-into-a-vector-in-matlab
+
