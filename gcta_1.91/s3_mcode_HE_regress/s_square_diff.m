e3eecbbb6df17db89c2dd7629be64e060de76e61
James Cai <jamescai@genomezoo.net>
2018-04-16 17:13:12 -0500

    .

--- gcta_1.91/s3_mcode_HE_regress/s_square_diff.m
+++ gcta_1.91/s3_mcode_HE_regress/s_square_diff.m
@@ -1,10 +1,11 @@
 function [X]=s_square_diff(y)
-n=length(y);
-X=zeros(n);
-
-for i=1:n-1
-    for j=i+1:n
-        %X(i,j)=(y(i)-y(j)).^2;
-        X(i,j)=(y(i)-y(j))*(y(i)-y(j));
-    end
-end
+% n=length(y);
+% X=zeros(n);
+% 
+% for i=1:n-1
+%     for j=i+1:n
+%         %X(i,j)=(y(i)-y(j)).^2;
+%         X(i,j)=(y(i)-y(j))*(y(i)-y(j));
+%     end
+% end
+X=(y-y').^2;
--- gcta_1.91/s3_mcode_HE_regress/s_square_diff_n_sum.m
+++ gcta_1.91/s3_mcode_HE_regress/s_square_diff_n_sum.m
@@ -1,18 +1,23 @@
 function [Xsd,Xss,Xcp]=s_square_diff_n_sum(y)
-n=length(y);
-Xsd=zeros(n);
-Xss=zeros(n);
-for i=1:n-1
-    for j=i+1:n
-        %X(i,j)=(y(i)-y(j)).^2;
-        Xsd(i,j)=(y(i)-y(j))*(y(i)-y(j));
-        Xss(i,j)=(y(i)+y(j))*(y(i)+y(j));
-    end
+
+Xsd=triu((y-y').^2,1);
+if nargout>1
+    Xss=triu((y+y').^2,1);
 end
-if nargout>2, 
+% n=length(y);
+% Xsd=zeros(n);
+% Xss=zeros(n);
+% for i=1:n-1
+%     for j=i+1:n
+%         %X(i,j)=(y(i)-y(j)).^2;
+%         Xsd(i,j)=(y(i)-y(j))*(y(i)-y(j));
+%         Xss(i,j)=(y(i)+y(j))*(y(i)+y(j));
+%     end
+% end
+if nargout>2
     if size(y,1)==1
-        Xcp=y'*y; 
+        Xcp=y'*y;
     else
-        Xcp=y*y'; 
+        Xcp=y*y';
     end
 end
