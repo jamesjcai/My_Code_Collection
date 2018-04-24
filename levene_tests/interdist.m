9ab83a7395e6e24091fc489706ba3205c7ae289b
James Cai <jamescai@genomezoo.net>
2018-04-24 01:05:47 -0500

    .

--- levene_tests/interdist.m
+++ levene_tests/interdist.m
@@ -1,7 +1,15 @@
-function D = interdist(A)
-  n=size(A,2);
-  for k=1:n
-    D(:,:,k)=A(:,k)-A(:,k)';
-  end
-  D=sqrt(sum(D.^2,3));
+function D = interdist(A,methodid)
+if nargin<2, methodid=2; end
+switch methodid
+    case 1
+        n=size(A,2);
+        for k=1:n
+            D(:,:,k)=A(:,k)-A(:,k)';
+        end
+        D=sqrt(sum(D.^2,3));
+    case 2
+        % D = sqrt(bsxfun(@plus,sum(A.^2)',sum(A.^2))-2*(A'*A));
+        % https://rundong.wordpress.com/2013/09/09/efficient-matlab-i-pairwise-distances/
+        D=sqrt(sum(A.^2,2)+sum(A.^2,2)'-2*(A*A'));
+end
 end
