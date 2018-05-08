cb07d0d72488207c7dbbe64d3d27f5da358dfe04
James Cai <jamescai@genomezoo.net>
2018-05-08 15:26:40 -0500

    .

--- levene_tests/test_anderson06_surface_res.m
+++ levene_tests/test_anderson06_surface_res.m
@@ -1,4 +1,5 @@
 load zxy Zc X Y
 Z=cellfun(@(X)(sum(X<0.001)),Zc);
-figure;
-mesh(X,Y,Z)
+figure; mesh(X,Y,Z)
+figure; contour(X,Y,Z,'showtext','on')
+% https://stackoverflow.com/questions/44816434/matlab-how-to-make-smooth-contour-plot
