e295a6bf67a56be3e8d5c5781a827c4ad6bcf31b
James Cai <jamescai@genomezoo.net>
2018-12-13 09:56:19 -0600

    .

--- singlecell_splinefit/i_myupdatefcn3.m
+++ singlecell_splinefit/i_myupdatefcn3.m
@@ -8,14 +8,8 @@ idx = event_obj.DataIndex;
 % i_plotsiglegene(idx,g);
 txt = {g(idx)};
 figure;
-if isempty(Y)
-    plot(X(idx,:));
-else
-%    subplot(2,1,1)
-%        stem(X(idx,:),'marker','none');
-%    subplot(2,1,2)
-%        stem(Y(idx,:),'marker','none');
-     stem(1:length(X(idx,:)), X(idx,:),'marker','none');
+stem(1:length(X(idx,:)), X(idx,:),'marker','none');
+if ~isempty(Y)
      hold on
      stem(1+length(X(idx,:)):length(Y(idx,:))+length(X(idx,:)),...
           Y(idx,:),'marker','none');
--- singlecell_splinefit/i_scatter_withinfo.m
+++ singlecell_splinefit/i_scatter_withinfo.m
@@ -1,9 +1,10 @@
-function i_scatter_withinfo(x,y,infotxt,X)
+function i_scatter_withinfo(x,y,infotxt,X,Y)
+if nargin<5, Y=[]; end
 if nargin<4, X=[]; end
-scatter(x,y);
+scatter(x,y,[],log(y)-log(x));
 dt = datacursormode;
 if isempty(X)
     dt.UpdateFcn = {@i_myupdatefcn1,infotxt};
 else
-    dt.UpdateFcn = {@i_myupdatefcn3,infotxt,X};
+    dt.UpdateFcn = {@i_myupdatefcn3,infotxt,X,Y};
 end
