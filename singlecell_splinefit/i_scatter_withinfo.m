5e4ba8493f2a9528ba6feea60d0979be68761aeb
James Cai <jamescai@genomezoo.net>
2018-12-18 10:37:41 -0600

    Update i_scatter_withinfo.m

--- singlecell_splinefit/i_scatter_withinfo.m
+++ singlecell_splinefit/i_scatter_withinfo.m
@@ -1,10 +1,10 @@
-function i_scatter_withinfo(x,y,infotxt,X,Y)
-if nargin<5, Y=[]; end
-if nargin<4, X=[]; end
-scatter(x,y,[],log(y)-log(x));
+function i_scatter_withinfo(d0,d1,infotxt,X0,X1)
+if nargin<5, X1=[]; end
+if nargin<4, X0=[]; end
+scatter(d0,d1,[],log(d1)-log(d0));
 dt = datacursormode;
-if isempty(X)
+if isempty(X0)
     dt.UpdateFcn = {@i_myupdatefcn1,infotxt};
 else
-    dt.UpdateFcn = {@i_myupdatefcn3,infotxt,X,Y};
+    dt.UpdateFcn = {@i_myupdatefcn3,infotxt,X0,X1};
 end
