d1c4801b374e5e8576081f7a2c30a0646d4e1094
Cai <jcai@cvm.tamu.edu>
2019-07-20 23:55:19 -0500

    .

--- oldfun/sctool/scde_test/test.R
+++ oldfun/sctool/scde_test/test.R
@@ -14,3 +14,35 @@ cd <- clean.counts(X)
 knn <- knn.error.models(cd)
 varinfo <- pagoda.varnorm(knn, counts = cd)
 
+#
+
+
+# load example dataset
+data(es.mef.small)
+
+# factor determining cell types
+sg <- factor(gsub("(MEF|ESC).*", "\\1", colnames(es.mef.small)), levels = c("ESC", "MEF"))
+# the group factor should be named accordingly
+names(sg) <- colnames(es.mef.small)  
+table(sg)
+
+# clean up the dataset
+cd <- clean.counts(es.mef.small, min.lib.size=1000, min.reads = 1, min.detected = 1)
+
+
+# EVALUATION NOT NEEDED
+# calculate models
+o.ifm <- scde.error.models(counts = cd, groups = sg, n.cores = 1, threshold.segmentation = TRUE, save.crossfit.plots = FALSE, save.model.plots = FALSE, verbose = 1)
+
+# filter out cells that don't show positive correlation with
+# the expected expression magnitudes (very poor fits)
+valid.cells <- o.ifm$corr.a > 0
+table(valid.cells)
+o.ifm <- o.ifm[valid.cells, ]
+
+# estimate gene expression prior
+o.prior <- scde.expression.prior(models = o.ifm, counts = cd, length.out = 400, show.plot = FALSE)
+
+
+
+
