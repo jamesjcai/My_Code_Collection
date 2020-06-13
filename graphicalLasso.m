9c9b086d3e3174a1a6412ee172e18422e05b33c6
James Cai <jamescai@genomezoo.net>
2020-06-13 14:44:12 -0500

    .

--- graphicalLasso.m
+++ graphicalLasso.m
@@ -3,7 +3,7 @@
 % Version: 2012-Feb
 % http://www.ece.ubc.ca/~xiaohuic/code/glasso/glasso.htm
 
-function [Theta W] = graphicalLasso(S, rho, maxIt, tol)
+function [Theta, W] = graphicalLasso(S, rho, maxIt, tol)
 
 % Solve the graphical Lasso
 % minimize_{Theta > 0} tr(S*Theta) - logdet(Theta) + rho * ||Theta||_1
@@ -35,11 +35,11 @@ W_old = W;
 i = 0;
 
 % Graphical Lasso loop
-while i < maxIt,
+while i < maxIt
     i = i+1;
-    for j = p:-1:1,
+    for j = p:-1:1
         jminus = setdiff(1:p,j);
-        [V D] = eig(W(jminus,jminus));
+        [V, D] = eig(W(jminus,jminus));
         d = diag(D);
         X = V * diag(sqrt(d)) * V'; % W_11^(1/2)
         Y = V * diag(1./sqrt(d)) * V' * S(jminus,j);    % W_11^(-1/2) * s_12
@@ -48,12 +48,12 @@ while i < maxIt,
         W(j,jminus) = W(jminus,j)';
     end
     % Stop criterion
-    if norm(W-W_old,1) < tol, 
+    if norm(W-W_old,1) < tol 
         break; 
     end
     W_old = W;
 end
-if i == maxIt,
+if i == maxIt
     fprintf('%s\n', 'Maximum number of iteration reached, glasso may not converge.');
 end
 
@@ -67,7 +67,7 @@ if nargin < 3, maxIt = 1e2; end
 
 % Initialization
 [n,p] = size(X);
-if p > n,
+if p > n
     b = zeros(p,1); % From the null model, if p > n
 else
     b = X \ Y;  % From the OLS estimate, if p <= n
@@ -80,14 +80,14 @@ XTX = X'*X;
 XTY = X'*Y;
 
 % Shooting loop
-while i < maxIt,
+while i < maxIt
     i = i+1;
-    for j = 1:p,
+    for j = 1:p
         jminus = setdiff(1:p,j);
         S0 = XTX(j,jminus)*b(jminus) - XTY(j);  % S0 = X(:,j)'*(X(:,jminus)*b(jminus)-Y)
-        if S0 > lambda,
+        if S0 > lambda
             b(j) = (lambda-S0) / norm(X(:,j),2)^2;
-        elseif S0 < -lambda,
+        elseif S0 < -lambda
             b(j) = -(lambda+S0) / norm(X(:,j),2)^2;
         else
             b(j) = 0;
@@ -97,6 +97,6 @@ while i < maxIt,
     if delta < tol, break; end
     b_old = b;
 end
-if i == maxIt,
+if i == maxIt
     fprintf('%s\n', 'Maximum number of iteration reached, shooting may not converge.');
 end
